09735791719b59733529b4af337c56ce1a7ce1ba
Domik0 <dafedot1@gmail.com>
2021-10-25 18:05:23 +0300

    Start create calendary

--- SchoolActivities/SchoolActivity.Designer.cs
+++ SchoolActivities/SchoolActivity.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\Users\Ilya\Source\Repos\Domik0\SchoolActivities\SchoolActivities\SchoolActivity.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\c#\SchoolActivities\SchoolActivities\SchoolActivity.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.

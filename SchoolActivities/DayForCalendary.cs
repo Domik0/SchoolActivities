378c3942a6a7013333b09d5f13a137f29279c893
Domik0 <dafedot1@gmail.com>
2021-10-26 01:00:58 +0300

    govnotablic

---
+++ SchoolActivities/DayForCalendary.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchoolActivities
+{
+    public class DayForCalendary
+    {
+        public DateTime? Day { get; set; }
+        public Dictionary<Circle, DateTime?> Circles { get; set; }
+
+        public DayForCalendary()
+        {
+            Day = null;
+            Circles = new Dictionary<Circle, DateTime?>();
+        }
+
+        public DayForCalendary(DateTime Day)
+        {
+            Circles = new Dictionary<Circle, DateTime?>();
+            this.Day = Day;
+        }
+    }
+}
--- SchoolActivities/TeacherMainPage.xaml.cs
+++ SchoolActivities/TeacherMainPage.xaml.cs
@@ -20,9 +20,75 @@ namespace SchoolActivities
     /// </summary>
     public partial class TeacherMainPage : Page
     {
+        private Teacher teacher;
+
         public TeacherMainPage(Teacher teacher)
         {
             InitializeComponent();
+            NamePrepod.DataContext = teacher;
+            this.teacher = teacher;
+            predmetComboBox.ItemsSource = this.teacher.Circles;
+            GenerateCalendary();
+        }
+
+        void GenerateCalendary()
+        {
+            List<DayForCalendary> days = new List<DayForCalendary>();
+
+            int nWeek = ((int)(new DateTime(DateTime.Today.Year, DateTime.Today.Month,1).DayOfWeek) + 6) %7;
+            
+            for (int i = 0; i < nWeek; i++)
+            {
+                days.Add(new DayForCalendary());
+            }
+            for (int i = 1; i <= DateTime.DaysInMonth(DateTime.Today.Year, DateTime.Today.Month); i++)
+            {
+                days.Add(new DayForCalendary(new DateTime(DateTime.Today.Year, DateTime.Today.Month, i)));
+            }
+
+
+            if (timeComboBox.SelectionBoxItem as string == "Месяц")
+            {
+                foreach (Circle circle in teacher.Circles)
+                {
+                    foreach (TimeTable time in circle.TimeTable)
+                    {
+                        foreach (DayForCalendary day in days)
+                        {
+                            if (day.Day != null)
+                            {
+                                if (day.Day.Value.Date == time.DateAndTime.Value.Date)
+                                {
+                                    day.Circles.Add(circle, day.Day);
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            else if((timeComboBox.SelectedItem as string) == "Неделя")
+            {
+                foreach (Circle circle in teacher.Circles)
+                {
+                    foreach (TimeTable time in circle.TimeTable)
+                    {
+                        foreach (DayForCalendary day in days)
+                        {
+                            if (day.Day == time.DateAndTime && day.Day > DateTime.Today.AddDays(-7) && day.Day < DateTime.Today.AddDays(7))
+                            {
+                                day.Circles.Add(circle, day.Day);
+                            }
+                        }
+                    }
+                }
+            }
+
+            CircleCalendary.ItemsSource = days;
+        } 
+
+        private void SelectDayClick(object sender, SelectionChangedEventArgs e)
+        {
+            throw new NotImplementedException();
         }
     }
 }

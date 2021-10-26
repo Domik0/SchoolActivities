ca6abbaa883b37c33a4e4f6d11dbf47bc2e679ed
Diistaf <kikopoint@mail.ru>
2021-10-26 09:46:50 +0300

    Sdelal Admin circles page and info him. FULL

---
+++ SchoolActivities/AdminCirclesPage.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Data.Entity;
+
+namespace SchoolActivities
+{
+    /// <summary>
+    /// Interaction logic for AdminCirclesPage.xaml
+    /// </summary>
+    public partial class AdminCirclesPage : Page
+    {
+        public AdminCirclesPage()
+        {
+            InitializeComponent();
+            UpdateListCircles();
+        }
+
+        private void CirclesList_MouseUp(object sender, MouseButtonEventArgs e)
+        {
+            Circle circle = circlesList.SelectedItem as Circle;
+            if (circle != null)
+            {
+                AdminMainPage.frame.Content = new AdminCirclesInfoPage(circle, this);
+            }
+        }
+
+        public void UpdateListCircles()
+        {
+            circlesList.ItemsSource = App.db.Circles.Include(r => r.Teacher).ToList();
+        }
+    }
+}

e92f4b6fbfd412bf06dbd9160e4d40e366af1f3d
Diistaf <kikopoint@mail.ru>
2021-10-27 09:09:37 +0300

    dead profile

--- SchoolActivities/AdminAllProfilePage.xaml.cs
+++ SchoolActivities/AdminAllProfilePage.xaml.cs
@@ -20,11 +20,33 @@ namespace SchoolActivities
     /// </summary>
     public partial class AdminAllProfilePage : Page
     {
+        Student dopStudent;
         public AdminAllProfilePage(Student student)
         {
             InitializeComponent();
+
+            dopStudent = student;
+
             gridData.DataContext = student;
             fioTextBox.Text = student.LastName + " " + student.FirstName + " " + student.Patronymic;
+
+            birthdayDatePicker.SelectedDate = student.Birthday;
+            classTextBox.Text = student.ClassGroup + " Класс";
+        }
+
+        private void SaveButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (fioTextBox.Text != "" && birthdayDatePicker.SelectedDate != null && classTextBox.Text != "")
+            {
+                Student student = App.db.Students.Where(s => s.Id == dopStudent.Id).FirstOrDefault();
+                string[] fioMas = fioTextBox.Text.Split(' ');
+
+                student.LastName = fioMas[0];
+                student.FirstName = fioMas[1];
+                student.Patronymic = fioMas[2];
+                student.Birthday = birthdayDatePicker.SelectedDate;
+                student.ClassGroup = classTextBox.Text;
+            }
         }
     }
 }

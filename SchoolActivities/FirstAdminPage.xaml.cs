75ac4c8e5b7301362bdacb28fea07b0f69347001
dialogbgr@mail.ru <dialogbgr@mail.ru>
2021-10-27 10:04:22 +0300

    Add async and diagram

--- SchoolActivities/FirstAdminPage.xaml.cs
+++ SchoolActivities/FirstAdminPage.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +13,10 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using LiveCharts;
+using LiveCharts.Defaults;
+using LiveCharts.Definitions.Series;
+using LiveCharts.Wpf;
 
 namespace SchoolActivities
 {
@@ -20,9 +25,26 @@ namespace SchoolActivities
     /// </summary>
     public partial class FirstAdminPage : Page
     {
+        public SeriesCollection SeriesViews { get; set; } = new SeriesCollection();
+
         public FirstAdminPage()
-        {
+        {           
             InitializeComponent();
+            Loaded += FirstAdminPage_Loaded;
+            pieChart.DataContext = this;
+        }
+
+        private void FirstAdminPage_Loaded(object sender, RoutedEventArgs e)
+        {
+            foreach (var circle in App.Circles)
+            {
+                SeriesViews.Add(new PieSeries
+                {
+                    Title = circle.Title,
+                    Values = new ChartValues<ObservableValue> { new ObservableValue(circle.Students.Count) },
+                    DataLabels = true
+                });
+            }
         }
     }
 }
